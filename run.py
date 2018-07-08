1b38655200a86c476024919b81996900b97c60bd
palash247 <palashkarmore@gmail.com>
2018-07-09 01:12:33 +0530

    heroku

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import app
 from db import db
-
+db.init_app(app)
 @app.before_first_request
 def create_table():
     import sqlite3
@@ -8,7 +8,7 @@ def create_table():
     cursor = connection.cursor()
     groups_table = 'create table if not exists groups (id integer primary key autoincrement, name text,chat_id integer, time_zone text, unique(chat_id))'
     messages_table = 'create table if not exists messages (id varchar(191) primary key, name text, chat_id integer, "text" text, schedule text, foreign key(chat_id) references groups(id))'
-    apscheduler_jobs_table = 'CREATE TABLE apscheduler_jobs(id VARCHAR(191) NOT NULL,next_run_time FLOAT,job_state BLOB NOT NULL,PRIMARY KEY(id))'
+    apscheduler_jobs_table = 'CREATE TABLE if not exists apscheduler_jobs(id VARCHAR(191) NOT NULL,next_run_time FLOAT,job_state BLOB NOT NULL,PRIMARY KEY(id))'
     user = 'create table if not exists user(id integer primary key autoincrement, username text unique not null, password not null)'
     delete_trigger = 'create trigger if not exists after delete on apscheduler_jobs BEGIN delete from messages where id=old.id; END;'
     cursor.execute(groups_table)
