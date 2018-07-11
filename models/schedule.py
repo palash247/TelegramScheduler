b2183b6dbc4d68d950363e1a20be0b681c32dff3
Palash Vishwas Karmore <palashkarmore@gmail.com>
2018-07-11 09:28:46 +0530

    added postgressql

---
+++ models/schedule.py
@@ -0,0 +1,11 @@
+
+from db import db
+from sqlalchemy.types import LargeBinary
+
+
+class SchedulModel(db.Model):
+
+    __tablename__ = "apscheduler_jobs"
+    id = db.Column('id', db.String(191), primary_key=True, nullable=False)
+    next_run_time = db.Column('next_run_time', db.Float, nullable=False)
+    job_state = db.Column('job_state', db.LargeBinary, nullable=False)
